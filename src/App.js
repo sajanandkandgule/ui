c4adfaa18855253f2c228a87803b1858dcd40aa9
sajanand <sajanandkandgule64@gmail.com>
2022-09-20 08:25:34 +0530

    uidesigen

---
+++ src/App.js
@@ -0,0 +1,237 @@
+import React from 'react'
+import img  from "./image/house.jpg"
+
+const App = (props) => {
+
+
+  return (
+    
+    <div className="col-md-12">
+    <div className="row">
+      
+      <div className="col-md-6">
+        
+          <div className="shadow-sm p-3 bg-body rounded">
+            <img src ={img}  style ={{ height:"300px" , width:"100%"}}/>
+            
+         
+        </div>
+      </div>
+    
+      <div className="col-md-3" style={{padding: "0px"}}>
+        <div className="row">
+          <div className="col-md-6">
+            <div className="shadow-sm p-3 bg-body rounded" style={{height: "160px"}}>
+              <h4>Box1</h4>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="shadow-sm p-3 bg-body rounded" style={{height: " 160px"}}>
+           <h4>box 2</h4>
+               
+            </div>
+          </div>
+        </div>
+    
+        <div className="row" style={{marginTop: "10px"}}>
+          <div className="col-md-12">
+            <div className="shadow-sm p-3 bg-body rounded" style={{height: "160px"}}>
+             <h4>build your dream home with us</h4>
+            </div>
+          </div>
+        </div>
+    
+      </div>
+    
+      <div className="col-md-3">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="shadow-sm p-1 bg-body rounded" style={{height: "160px"}}>
+              <h4>Box4</h4>
+            </div>
+          </div>
+          <div className="col-md-12" style={{marginTop: "10px"}}>
+            <div className="shadow-sm p-1 bg-body rounded" style={{height: "160px"}}>
+              <h4>Emi option is available book now</h4>
+          </div>
+        </div>
+
+      </div>
+      </div>
+    
+      <div className="row" style={{marginTop: "10px"}}>
+        <div className="col-md-3" style={{padding: "2px"}}>
+         <div className="shadow-sm p-1 bg-body rounded" style={{height: "150px"}}>
+         <div className="row" style={{marginLeft: "10px"}}>
+           <div className="col-md-8" style={{backgroundColor: "#7ec17e"}}>
+               <h4>house</h4>
+             </div>
+           <div className="col-md-4">
+               <h4>one</h4>
+           </div>
+         </div>
+         <div className="row" style={{marginTop: "40px"}}>
+           <div className="col-md-4">
+               <h4>room</h4>
+             </div>
+           <div className="col-md-4">
+               <h4>2bk</h4>
+           </div>
+           <div className="col-md-4">
+             <h4>10</h4>
+         </div>
+         </div>
+         </div>
+         
+       </div>
+       <div className="col-md-3" style={{padding: "2px"}}>
+         <div className="shadow-sm p-1 bg-body rounded" style={{height: "150px"}}>
+         <div className="row" style={{marginLeft: "10px"}}>
+           <div className="col-md-8" style={{backgroundColor: "#7ec17e"}}>
+               <h4>house</h4>
+             </div>
+           <div className="col-md-4">
+               <h4>two</h4>
+           </div>
+         </div>
+         <div className="row" style={{marginTop: "40px"}}>
+           <div className="col-md-4">
+               <h4>room</h4>
+             </div>
+           <div className="col-md-4">
+               <h4>3bhk</h4>
+           </div>
+           <div className="col-md-4">
+             <h4>20</h4>
+         </div>
+         </div>
+         </div>
+         
+       </div>
+       <div className="col-md-3" style={{padding: "2px"}}>
+         <div className="shadow-sm p-1 bg-body rounded" style={{height: "150px"}}>
+         <div className="row" style={{marginLeft: "10px"}}>
+           <div className="col-md-8" style={{backgroundColor: "#7ec17e"}}>
+               <h4>house</h4>
+             </div>
+           <div className="col-md-4">
+               <h4>three</h4>
+           </div>
+         </div>
+         <div className="row" style={{marginTop: "40px"}}>
+           <div className="col-md-4">
+               <h4>room</h4>
+             </div>
+           <div className="col-md-4">
+               <h4>1bhk</h4>
+           </div>
+           <div className="col-md-4">
+             <h4>10</h4>
+         </div>
+         </div>
+         </div>
+         
+       </div>
+       <div className="col-md-3" style={{padding: "2px"}}>
+         <div className="shadow-sm p-1 bg-body rounded" style={{height: "150px"}}>
+           <h4>Hurry-up booking are open</h4>
+         </div>
+      
+       
+     </div>
+     </div>
+    
+        <div className="row" style={{marginTop: "10px"}}>
+        
+          <div className="col-md-3" style={{padding: "2px"}}>
+           <div className="shadow-sm p-1 bg-body rounded" style={{height: "150px"}}>
+           <div className="row" style={{marginLeft: "10px"}}>
+             <div className="col-md-8" style={{backgroundColor: "#7ec17e"}}>
+                 <h4>house</h4>
+               </div>
+             <div className="col-md-4">
+                 <h4>four</h4>
+             </div>
+           </div>
+           <div className="row" style={{marginTop: "40px"}}>
+             <div className="col-md-4">
+                 <h4>room</h4>
+               </div>
+             <div className="col-md-4">
+                 <h4>2.5bhk</h4>
+             </div>
+             <div className="col-md-4">
+               <h4>10</h4>
+           </div>
+           </div>
+           </div>
+           
+         </div>
+         <div className="col-md-3" style={{padding: "2px"}}>
+           <div className="shadow-sm p-1 bg-body rounded" style={{height: "150px"}}>
+           <div className="row" style={{marginLeft: "10px"}}>
+             <div className="col-md-8" style={{backgroundColor: "#7ec17e"}}>
+                 <h4>house</h4>
+               </div>
+             <div className="col-md-4">
+                 <h4>five</h4>
+             </div>
+           </div>
+           <div className="row" style={{marginTop: "40px"}}>
+             <div className="col-md-4">
+                 <h4>room</h4>
+               </div>
+             <div className="col-md-4">
+                 <h4>4bhk</h4>
+             </div>
+             <div className="col-md-4">
+               <h4>8</h4>
+           </div>
+           </div>
+           </div>
+           
+         </div>
+         <div className="col-md-3" style={{padding: "2px"}}>
+           <div className="shadow-sm p-1 bg-body rounded" style={{height: "150px"}}>
+           <div className="row" style={{marginLeft: "10px"}}>
+             <div className="col-md-8" style={{backgroundColor: "#7ec17e"}}>
+                 <h4>house</h4>
+               </div>
+             <div className="col-md-4">
+                 <h4>six</h4>
+             </div>
+           </div>
+           <div className="row" style={{marginTop: "40px"}}>
+             <div className="col-md-4">
+                 <h4>room</h4>
+               </div>
+             <div className="col-md-4">
+                 <h4>3.5bhk</h4>
+             </div>
+             <div className="col-md-4">
+               <h4>10</h4>
+           </div>
+           </div>
+           </div>
+           
+         </div>
+         <div className="col-md-3" style={{padding: "2px"}}>
+           <div className="shadow-sm p-1 bg-body rounded" style={{height: "150px"}}>
+             <h4>Box</h4>
+           </div>
+        
+         
+       </div>
+       </div>
+    
+    </div>
+   </div>
+
+  )
+}
+
+export default App
+
+
+
+
